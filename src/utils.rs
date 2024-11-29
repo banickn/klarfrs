f2c2a3248af9f2e6b04addb8f82fc22c2e22dc4d
Norman Banick <norman.banick@gmail.com>
2024-11-29 09:31:09 +0000

    Refactor to utils

---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+pub fn is_datetime_valid(datetime_str: &str) -> bool {
+    static DATETIME_REGEX: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"^\d{2}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$").unwrap());
+    DATETIME_REGEX.is_match(datetime_str)
+}
